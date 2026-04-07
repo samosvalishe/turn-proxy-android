ffa0aab15bec48a5e4168e9e6c60726cbda1f1d4
samosvalishe <samosvalishe@users.noreply.github.com>
2026-04-07 11:35:11 +0300

    fix: bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.freeturn.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 8
-        versionName = "1.3.0"
+        versionCode = 9
+        versionName = "1.3.1"
     }
 
     packaging {
